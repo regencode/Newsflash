0169ab2e4ca058d089a38bb6d4ddffcd83ac9268
Rudessss <152311496+Rudessss@users.noreply.github.com>
2024-06-03 16:52:02 +0700

    change of file name
    Navigation change of file for Home, Explore and Profile

---
+++ app/(tabs)/Explore.jsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View, ScrollView } from 'react-native'
+import React from 'react'
+import NewsUnit from '../../components/newsUnit'
+import customHeader from '../../components/customHeader'
+
+const Explore = () => {
+  return (
+    <ScrollView stickyHeaderIndices={[0]} className="bg-main">
+    {customHeader(true)}
+      <Text className="text-3xl ml-[5%] mt-4 mb-4 font-proxima">Explore</Text>
+      <NewsUnit
+      title="Lorem ipsum dolor sit amet"
+      author="Generic Writer"
+      date="02-02-24"
+      tags="business"
+      />
+      <NewsUnit
+      title="Lorem ipsum dolor sit amet"
+      author="Generic Writer"
+      date="02-02-24"
+      tags="business"
+      />
+      <NewsUnit
+      title="Lorem ipsum dolor sit amet"
+      author="Generic Writer"
+      date="02-02-24"
+      tags="business"
+      />
+    </ScrollView>
+  )
+}
+
+export default Explore
